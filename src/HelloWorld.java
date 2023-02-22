741abef0a43903d6ad1232e4fac8b37bccccd3e6
Julio Jaimes <carlosjuliojr@hotmail.com>
2023-02-21 22:59:53 -0300

    variables

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,27 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Julio Jaimes R.");
+
+        /*
+        Reference of data
+         */
+        //String
+        String name;
+        name = "Hello: ";
+        //Creation variable number type;
+        Number age = 20;
+        /*
+         Primitive Variable
+         */
+        int year = 2023;
+        float temperature = 25.3F;
+        double stature = 1.67;
+
+
+        System.out.println(name.toUpperCase() + "Julio Jaimes R.");
+        System.out.println("Age: " + age);
+        System.out.println("year: " + year);
+        System.out.println("Temperature: " + temperature);
+        System.out.println("Stature: " + stature);
+
     }
 }
